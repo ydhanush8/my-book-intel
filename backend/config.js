2cc3ecdecff4b2afb29acae2d5a0605fc0dc2422
Dhanush <90715682+ydhanush8@users.noreply.github.com>
2024-02-14 16:15:19 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -2,6 +2,3 @@ export const PORT = process.env.PORT || 5555;
 
 export const mongoDBURL =
   "mongodb+srv://<username>:<password>@cluster0.gc5das7.mongodb.net/books-collection?retryWrites=true&w=majority";
-
-// Please create a free database for yourself.
-// This database will be deleted after tutorial
